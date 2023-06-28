d5ccf2e1bfbfcd786be3c047c6397fd2624ee6ad
victor <duprezvictor4@gmail.com>
2023-06-29 01:18:12 +0200

    first commit

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,7 +18,7 @@ Spree.config do |config|
   # config.inventory_cache_threshold = 3
 
   # Configure adapter for attachments on products and taxons (use ActiveStorageAttachment or PaperclipAttachment)
-  config.active_storage.service = :google
+  config.image_attachment_module = 'Spree::Image::ActiveStorageAttachment'
   config.taxon_attachment_module = 'Spree::Taxon::ActiveStorageAttachment'
 
   # Defaults
