a020a08b649e5e08b049706e6ed6398649ec473a
victor <duprezvictor4@gmail.com>
2023-07-01 13:43:24 +0200

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,4 @@ class HomeController < StoreController
     @searcher = build_searcher(params.merge(include_images: true))
     @products = @searcher.retrieve_products
   end
-
-  private
-
-  def cache_key_for_products
-    count          = @products.count
-    max_updated_at = @products.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "products/all-#{count}-#{max_updated_at}"
-  end
 end
