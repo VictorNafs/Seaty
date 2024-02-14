c3e4097f96da86d77c326fc34cbfc80cc4628a84
victor <duprezvictor4@gmail.com>
2024-02-14 23:20:15 +0100

    panier commit

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -14,23 +14,24 @@ class CartsController < StoreController
   # Shows the current incomplete order from the session
   def edit
     @order = current_order(build_order_if_necessary: true)
+    check_and_remove_unavailable_items(@order)
     authorize! :edit, @order, cookies.signed[:guest_token]
-  
-    # Nouvelle logique pour vérifier la disponibilité des créneaux horaires
-    @order.line_items.each do |line_item|
-      product = line_item.variant.product
-      unless product.available? # Vous devez implémenter cette méthode `available?` dans votre modèle Product
-        @order.contents.remove(line_item.variant, line_item.quantity)
-        flash[:notice] = "Certains articles n'étaient plus disponibles et ont été retirés de votre panier."
-      end
-    end
-  
     if params[:id] && @order.number != params[:id]
       flash[:error] = t('spree.cannot_edit_orders')
       redirect_to edit_cart_path
     end
   end
-  
+
+  private
+
+  def check_and_remove_unavailable_items(order)
+    order.line_items.each do |item|
+      unless item.variant.product.available? # Vous devez définir la méthode `available?` dans votre modèle de produit.
+        order.contents.remove(item.variant, item.quantity)
+        flash[:notice] = "Certains articles n'étaient plus disponibles et ont été retirés de votre panier."
+      end
+    end
+  end
 
   def update
     authorize! :update, @order, cookies.signed[:guest_token]
